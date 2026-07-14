d34100023ee4725486496350dd5784788b810831
Miguel203Sopa <migue.guevara989@gmail.com>
2026-07-13 19:17:14 -0600

    refactor: Guia4

---
+++ cart-react/app/page.tsx
@@ -0,0 +1,12 @@
+import ProductGrid from "@/components/ProductGrid";
+
+export default function Home() {
+  return (
+    <main className="home-container">
+      <br />
+      <section>
+        <ProductGrid />
+      </section>
+    </main>
+  );
+}
---
+++ cart-react/data/books.ts
@@ -0,0 +1,46 @@
+import { Product } from "../types/Product";
+
+export const books: Product[] =[
+{
+    id: 1,
+    title: "Cien Años de Soledad",
+    price:15,
+    image: 'https://images.penguinrandomhouse.com/cover/9780525562443',
+    quantity: 2
+},
+
+{
+    id: 2,
+    title: 'El Señor de los Anillos(Trilogia)',
+    price: 25,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 120
+},
+{
+    id: 3,
+    title: 'Cuentos de Barro',
+    price: 24,
+    image: 'https://www.librosdelaballena.com/wp-content/uploads/2020/05/cuentos-barro-244x300.png',
+    quantity: 22
+
+},
+
+{
+    id: 4,
+    title: 'Tierra de Infancia',
+    price: 25,
+    image: 'https://assets.isu.pub/document-structure/230605011415-591c5b47dde190303e7de29a0a888041/v1/fa8a209bb2ce609861c6572f117e69f8.jpeg?width=720&quality=85%2C50',
+    quantity: 2
+
+},
+
+{
+    id: 5,
+    title: 'Harry Potter Pack',
+    price:390,
+    image: 'https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_com=1113',
+    quantity: 43
+}
+
+
+]
